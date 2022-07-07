134a7f21f87582fb2992c21318c8dbb3c51e49b1
ItamarSahar <91605437+ItamarSahar@users.noreply.github.com>
2022-07-07 13:26:27 +0300

    Add some elemnts

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import {
+	StyleSheet,
+	Text,
+	View,
+	TouchableHighlight,
+	SafeAreaView,
+	Image,
+	Button,
+	Alert,
+} from 'react-native'
 
 export default function App() {
 	let x = 1
 	return (
-		<View style={styles.container}>
+		<SafeAreaView style={styles.container}>
 			<Text>Hello World!</Text>
-			<StatusBar style="auto" />
-		</View>
+			<Button
+				color="orange"
+				title="Click me !"
+				onPress={() => {
+					Alert.alert('My title', 'My message', [
+						{ text: 'Yes' },
+						{ text: 'No' },
+					])
+				}}
+			/>
+			<TouchableHighlight
+				onPress={() => {
+					console.log('Touch!')
+				}}
+			>
+				<Image
+					fadeDuration={2000} //!Only in andoid !
+					source={{
+						width: 200,
+						height: 300,
+						uri: 'https://picsum.photos/200/300',
+					}}
+				></Image>
+			</TouchableHighlight>
+		</SafeAreaView>
 	)
 }
 
